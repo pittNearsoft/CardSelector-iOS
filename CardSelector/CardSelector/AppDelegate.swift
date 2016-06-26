9ea56df965f89bcae57d9d28317cca7bf34a3378
pittNearsoft <projas@nearsoft.com>
2016-06-26 14:29:36 -0700

    Add setup for facebook login

--- CardSelector/CardSelector/AppDelegate.swift
+++ CardSelector/CardSelector/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FBSDKCoreKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -39,6 +40,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func applicationWillTerminate(application: UIApplication) {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+  }
+  
+  func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+    return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
   }
 
 
--- CardSelector/CardSelector/ViewController.swift
+++ CardSelector/CardSelector/ViewController.swift
@@ -7,19 +7,17 @@
 //
 
 import UIKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    let loginButton = FBSDKLoginButton()
+    loginButton.center = view.center
+    view.addSubview(loginButton)
   }
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
 
 }
 
