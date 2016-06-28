4a44541d6abe358a8e0b7564fca300c52a159d4a
pittNearsoft <projas@nearsoft.com>
2016-06-28 16:02:44 -0700

    Add User to Models

---
+++ CardSelector/CardSelector/User.swift
@@ -0,0 +1,33 @@
+//
+//  User.swift
+//  CardSelector
+//
+//  Created by projas on 6/28/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+import Google
+import GoogleSignIn
+
+class User: Object {
+  dynamic var userId = ""
+  dynamic var tokenId = ""
+  dynamic var name = ""
+  dynamic var email = ""
+  dynamic var imageUrl = ""
+  
+  convenience init(WithGoogleUser googleUser: GIDGoogleUser) {
+    self.init()
+    userId = googleUser.userID
+    tokenId = googleUser.authentication.idToken
+    name = googleUser.profile.name
+    email = googleUser.profile.email
+    imageUrl = googleUser.profile.imageURLWithDimension(200).absoluteString
+  }
+  
+  override static func primaryKey() -> String?{
+    return "userId"
+  }
+}
