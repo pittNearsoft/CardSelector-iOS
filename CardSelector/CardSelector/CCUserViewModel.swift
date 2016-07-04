6cda40898e872314f118dab2059accae4ab76040
pittNearsoft <projas@nearsoft.com>
2016-07-03 23:59:35 -0700

    Remove Card prefix for CC (card Compadre)

---
+++ CardSelector/CardSelector/CCUserViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CCUserViewModel.swift
+//  CardSelector
+//
+//  Created by projas on 7/1/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CCUserViewModel: NSObject {
+
+}
---
+++ CardSelector/CardSelector/SessionTestManager.swift
@@ -0,0 +1,14 @@
+//
+//  sessionTestManager.swift
+//  CardSelector
+//
+//  Created by projas on 6/30/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+protocol SessionTestManager {
+  static func signIn() -> Void
+  static func signOut() -> Void
+}
