dcdb3bb988cada5567fa2eaa9ad44eeb4ad39482
pittNearsoft <projas@nearsoft.com>
2016-07-06 17:29:47 -0700

    Add MarketInfoView and CCPlace model

---
+++ CardSelector/CardSelector/MarkerInfoView.swift
@@ -0,0 +1,21 @@
+//
+//  MarkerInfoView.swift
+//  CardSelector
+//
+//  Created by projas on 7/6/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class MarkerInfoView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
