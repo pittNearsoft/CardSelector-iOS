7db8f76e598da2a9d582ce43c10e4ae2ee4b5dea
pittNearsoft <projas@nearsoft.com>
2016-07-07 14:36:25 -0700

    Implement methods to map JSON to Coordinate for CCPlace

--- CardSelector/CardSelector/CCCoordinateTransform.swift
+++ CardSelector/CardSelector/CCCoordinateTransform.swift
@@ -6,8 +6,28 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import ObjectMapper
+import CoreLocation
 
-class CCCoordinateTransform: NSObject {
+class CCCoordinateTransform: TransformType {
+
+  func transformFromJSON(value: AnyObject?) -> CLLocationCoordinate2D? {
+    
+    guard let coordinate = value as? [String: Double] else { return nil}
+    
+    return CLLocationCoordinate2D(latitude: coordinate["lat"]!, longitude: coordinate["lng"]!)
+  }
+  
+  func transformToJSON(value: CLLocationCoordinate2D?) -> [String: Double]?{
+    if let coordinate = value {
+      return [
+        "lat": coordinate.latitude,
+        "lng": coordinate.longitude
+      ]
+    }
+    
+    return nil
+  }
 
 }
