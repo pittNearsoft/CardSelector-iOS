d5348dd83d9595bf404f067d4b423dcf9ac1159f
pittNearsoft <projas@nearsoft.com>
2016-07-06 17:30:14 -0700

    Add Label and photo to markerInfoView

--- CardSelector/CardSelector/MarkerInfoView.swift
+++ CardSelector/CardSelector/MarkerInfoView.swift
@@ -10,12 +10,8 @@ import UIKit
 
 class MarkerInfoView: UIView {
 
-    /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
+  @IBOutlet weak var nameLabel: UILabel!
+  @IBOutlet weak var placePhoto: UIImageView!
+    
 
 }
