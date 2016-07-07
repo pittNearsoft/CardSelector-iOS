840d83d600b3666c9d0f664955e05c7bdc2ce5cf
pittNearsoft <projas@nearsoft.com>
2016-07-07 14:37:13 -0700

    Implement methods and properties to map JSON

--- CardSelector/CardSelector/CCPlace.swift
+++ CardSelector/CardSelector/CCPlace.swift
@@ -6,8 +6,30 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import CoreLocation
+import ObjectMapper
 
-class CCPlace: NSObject {
 
+class CCPlace: Mappable {
+  var name = ""
+  var address = ""
+  var coordinate = CLLocationCoordinate2D()
+  var placeType = ""
+  var photo_reference = ""
+  var photo = UIImage(named: "generic")
+  
+  required init?(_ map: Map) {
+    
+  }
+  
+  func mapping(map: Map) {
+    name        <- map["name"]
+    address     <- map["vicinity"]
+    coordinate  <- (map["geometry.location"], CCCoordinateTransform())
+    placeType   <- (map["types"],CCPlaceTypeTransform())
+    photo_reference    <- map["photos.0.photo_reference"]
+  }
+  
 }
+
