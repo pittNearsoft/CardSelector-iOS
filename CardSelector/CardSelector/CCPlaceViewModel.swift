006eb4856ba6a751e30795ad84b7d2b77513734e
pittNearsoft <projas@nearsoft.com>
2016-07-07 15:16:45 -0700

    Add CCPlaceViewModel to fetch nearby places

---
+++ CardSelector/CardSelector/CCPlaceViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  CCPlaceViewModel.swift
+//  CardSelector
+//
+//  Created by projas on 7/7/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+import ObjectMapper
+
+class CCPlaceViewModel {
+  
+  func fetchNearbyPlacesWithCoordinate(coordinate: CLLocationCoordinate2D, radius: Double, types: [String], completion: (places: [CCPlace])->Void) {
+    <#function body#>
+  }
+}
