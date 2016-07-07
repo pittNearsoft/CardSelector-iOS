831003fd63a4a15b2070d754a50ec6afd58eeacd
pittNearsoft <projas@nearsoft.com>
2016-07-07 15:38:23 -0700

    Add CCPlaceService

---
+++ CardSelector/CardSelector/CCPlaceService.swift
@@ -0,0 +1,13 @@
+//
+//  CCPlaceService.swift
+//  CardSelector
+//
+//  Created by projas on 7/7/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CCPlaceService: NSObject {
+
+}
