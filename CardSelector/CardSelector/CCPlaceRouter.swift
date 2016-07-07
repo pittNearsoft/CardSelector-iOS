f12266ce8f7bd25c8d6bae24d02ba1d097b55dfd
pittNearsoft <projas@nearsoft.com>
2016-07-07 15:55:38 -0700

    Configure CCPlaceRouter to establish connection with Google Places API

--- CardSelector/CardSelector/CCPlaceRouter.swift
+++ CardSelector/CardSelector/CCPlaceRouter.swift
@@ -6,8 +6,35 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
+import Alamofire
+import CoreLocation
 
-class CCPlaceRouter: NSObject {
+enum CCPlaceRouter: URLRequestConvertible {
+  case fetchNearbyPlacesWithCoordinate(coodinate: CLLocationCoordinate2D, radius: Double)
+  
+  var method: Alamofire.Method{
+    switch self {
+    case .fetchNearbyPlacesWithCoordinate:
+      return .GET
+    }
+  }
+  
+  var path: String{
+    switch self {
+    case .fetchNearbyPlacesWithCoordinate(let coordinate, let radius):
+      return "place/nearbysearch/json?location=\(coordinate.latitude),\(coordinate.longitude)&radius=\(radius)&rankby=prominence&sensor=true"
+    }
+    
+    
+  }
+  
+  var URLRequest: NSMutableURLRequest{
+    let url = APIClient.getFullUrlWithPath(path)
+    let mutableURLRequest = NSMutableURLRequest(URL: url)
+    
+    mutableURLRequest.HTTPMethod = method.rawValue
+    
+    return mutableURLRequest
+  }
 
 }
