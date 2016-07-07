7bdfa1983b4e5c9872c403d6f6b33a33c991cd90
pittNearsoft <projas@nearsoft.com>
2016-07-07 14:37:55 -0700

    Add initializer to build a PlaceMarker on the map

--- CardSelector/CardSelector/CCPlaceMarker.swift
+++ CardSelector/CardSelector/CCPlaceMarker.swift
@@ -6,8 +6,19 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import GoogleMaps
 
-class CCPlaceMarker: NSObject {
-
+class CCPlaceMarker: GMSMarker {
+  let place: CCPlace
+  
+  init(place: CCPlace){
+    self.place = place
+    super.init()
+    
+    position = place.coordinate
+    icon = UIImage(named: place.placeType+"_pin")
+    groundAnchor = CGPoint(x: 0.5, y: 1)
+    appearAnimation = kGMSMarkerAnimationPop
+  }
 }
