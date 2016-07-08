6d9469098784cdfdb970127e71adf0f805605363
pittNearsoft <projas@nearsoft.com>
2016-07-07 17:45:37 -0700

    Replace Dictionary for Array casting and add types parameter

--- CardSelector/CardSelector/CCPlaceService.swift
+++ CardSelector/CardSelector/CCPlaceService.swift
@@ -10,15 +10,15 @@ import Alamofire
 import CoreLocation
 
 class CCPlaceService {
-  let apiClient = APIClient()
+  private let apiClient = APIClient()
   
-  func fetchNearbyPlacesWithCoordinate(coordinate: CLLocationCoordinate2D, radius: Double, types: [String], completion: (jsonPlaces: [String: AnyObject]) -> Void, onError: (error: NSError)->Void) {
+  func fetchNearbyPlacesWithCoordinate(coordinate: CLLocationCoordinate2D, radius: Double, types: [String], completion: (jsonPlaces: [AnyObject]) -> Void, onError: (error: NSError)->Void) {
     
     apiClient.manager.request(CCPlaceRouter.fetchNearbyPlacesWithCoordinate(coodinate: coordinate, radius: radius, types: types))
       .CCresponseJSON { (response) in
         switch response.result{
           case .Success(let JSON):
-            guard let result = JSON as? [String: AnyObject] else{
+            guard let result = JSON["results"] as? [AnyObject] else{
               onError(error: Error.error(code: -1, failureReason: "Bad json received"))
               return
             }
