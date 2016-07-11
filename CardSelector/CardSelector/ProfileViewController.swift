772944ce7539b57ffc66e047e3db33563c813346
pittNearsoft <projas@nearsoft.com>
2016-07-11 01:27:05 -0700

    Add better UI for ProfileViewController

--- CardSelector/CardSelector/ProfileViewController.swift
+++ CardSelector/CardSelector/ProfileViewController.swift
@@ -16,7 +16,9 @@ class ProfileViewController: UIViewController {
   
   @IBOutlet weak var userNameLabel: UILabel!
   @IBOutlet weak var emailLabel: UILabel!
+  @IBOutlet weak var profileImageView: UIImageView!
   
+  @IBOutlet weak var signOutButton: UIButton!
   let user = CCUserViewModel.getLoggedUser()
   
   
@@ -24,6 +26,16 @@ class ProfileViewController: UIViewController {
     super.viewDidLoad()
     userNameLabel.text = user!.name
     emailLabel.text = user!.email
+    
+    profileImageView.layer.cornerRadius = profileImageView.frame.height/2
+    profileImageView.layer.masksToBounds = true
+    
+    profileImageView.layer.borderColor = UIColor.whiteColor().CGColor
+    profileImageView.layer.borderWidth = 5.0
+    
+    signOutButton.layer.cornerRadius = 5
+    signOutButton.clipsToBounds = true
+    
   }
   
   
