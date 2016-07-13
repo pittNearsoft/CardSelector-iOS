fc5cf063d9950a78636eb88bfa1b29ec3d7df7fd
pittNearsoft <projas@nearsoft.com>
2016-07-12 23:54:13 -0700

    Add CardView to reuse credit card form

---
+++ CardSelector/CardSelector/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  CardView.swift
+//  CardSelector
+//
+//  Created by projas on 7/12/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
