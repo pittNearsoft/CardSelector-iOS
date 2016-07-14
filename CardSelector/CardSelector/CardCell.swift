2bb7c0920b0ade93bbfdc76d1ecfee575427cb33
pittNearsoft <projas@nearsoft.com>
2016-07-14 08:00:26 -0700

    Add CardView to reuse credit card if necessary

--- CardSelector/CardSelector/CardCell.swift
+++ CardSelector/CardSelector/CardCell.swift
@@ -17,6 +17,31 @@ class CardCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
       cardView.roundCorners()
+      
+//      let card = CardView(frame: CGRect(x: 0, y: 0, width: 338, height: 168))
+//      
+//      addSubview(card)
+//      
+//      card.backgroundColor = UIColor.whiteColor()
+//      
+//      
+      //self.frame = CGRect(x: 0, y: 0, width: 359, height: 184)
+      
+      
+//      let card = NSBundle.mainBundle().loadNibNamed("CardView", owner: self, options: nil).first as! CardView
+//      card.roundCorners()
+////      card.frame = CGRect(x: 0, y: 0, width: 338, height: 168)
+//      
+//      addSubview(card)
+//      
+//      
+//      
+//      addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]-8-|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": card]))
+//      
+//      addConstraint(NSLayoutConstraint(item: card, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0 ) )
+
+      
+      //card.center = self.center
     }
   
   static func reuseIdentifier() -> String {
--- CardSelector/CardSelector/CardView.swift
+++ CardSelector/CardSelector/CardView.swift
@@ -17,5 +17,7 @@ class CardView: UIView {
         // Drawing code
     }
     */
+  
+
 
 }
