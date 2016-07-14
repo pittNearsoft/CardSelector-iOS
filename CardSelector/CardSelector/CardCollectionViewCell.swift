722abeff43c9eaaa41e6dd56f011cbfe9bc6629a
pittNearsoft <projas@nearsoft.com>
2016-07-14 08:48:43 -0700

    Add CardCollectionViewCell

---
+++ CardSelector/CardSelector/CardCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CardCollectionViewCell.swift
+//  CardSelector
+//
+//  Created by projas on 7/14/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CardCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
