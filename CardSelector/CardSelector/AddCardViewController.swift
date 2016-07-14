35ff74afeece57e913c82a0e616bce8770ac28ed
pittNearsoft <projas@nearsoft.com>
2016-07-14 11:44:33 -0700

    Configure card collection view for AddCardViewController

--- CardSelector/CardSelector/AddCardViewController.swift
+++ CardSelector/CardSelector/AddCardViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class AddCardViewController: UIViewController {
   
+  @IBOutlet weak var cardCollectionView: UICollectionView!
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    cardCollectionView.dataSource = self
+    cardCollectionView.delegate = self
+    
+    let nibName = UINib(nibName: CardCollectionViewCell.reuseIdentifier(), bundle:nil)
+    cardCollectionView.registerNib(nibName, forCellWithReuseIdentifier: CardCollectionViewCell.reuseIdentifier())
     
   }
   
+  @IBAction func saveCard(sender: AnyObject) {
+    print("Save data here!")
+  }
   
+  
+  @IBAction func cancelOperation(sender: AnyObject) {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+  
+}
+
+
+extension AddCardViewController: UICollectionViewDataSource{
+  func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+    return 1
+  }
+  
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 5
+  }
+  
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = cardCollectionView.dequeueReusableCellWithReuseIdentifier(CardCollectionViewCell.reuseIdentifier(), forIndexPath: indexPath)
+    
+    return cell
+  }
+}
+
+extension AddCardViewController: UICollectionViewDelegate{
+  
+}
+
+extension AddCardViewController: UICollectionViewDelegateFlowLayout{
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
+    return 0
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
+    return 0
+  }
+  
+  func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+    return CGSize(width: cardCollectionView.frame.width, height: cardCollectionView.frame.height)
+  }
 }
--- CardSelector/CardSelector/CardCollectionViewCell.swift
+++ CardSelector/CardSelector/CardCollectionViewCell.swift
@@ -9,10 +9,38 @@
 import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    
+    
+    
+    
+    let card = NSBundle.mainBundle().loadNibNamed("CardView", owner: self, options: nil).first as! CardView
+    card.roundCorners()
+    //card.frame = CGRect(x: 0, y: 0, width: 338, height: 168)
+    
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
+    //card.frame = CGRect(x: 0, y: 0, width: frame.width-40, height: frame.height-80)
+    card.frame = CGRect(x: 0, y: 0, width: frame.width*(0.88), height: frame.height*(0.48))
+    
+    addSubview(card)
+    
+    
+    
+//    addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": card]))
+//    
+//    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0 ) )
+//    
+//    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0 ) )
+    
+    
+    card.center = self.center
+  }
+  
+  static func reuseIdentifier() -> String {
+    return "CardCollectionViewCell"
+  }
+  
 }
