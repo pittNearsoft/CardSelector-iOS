f12008b2fe17bb1ca0adb4e66fa5bc7dd42af779
pittNearsoft <projas@nearsoft.com>
2016-07-16 16:29:42 -0700

    create IBOulets for bank image, color, and card program. Also rename card to cardView

--- CardSelector/CardSelector/CardView.swift
+++ CardSelector/CardSelector/CardView.swift
@@ -10,20 +10,32 @@ import UIKit
 
 class CardView: UIView {
 
-  @IBOutlet var card: UIView!
+  @IBOutlet var cardView: UIView!
+  
+  @IBOutlet weak var bankImage: UIImageView!
+  @IBOutlet weak var background: UIView!
+  @IBOutlet weak var programLabel: UILabel!
+  
+  
   
   required init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
     
     NSBundle.mainBundle().loadNibNamed("CardView", owner: self, options: nil)
-    addSubview(card)
-    card.translatesAutoresizingMaskIntoConstraints = false
+    addSubview(cardView)
+    cardView.translatesAutoresizingMaskIntoConstraints = false
 
     
-    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0 ))
+    addConstraint(NSLayoutConstraint(item: cardView, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0 ))
     
-    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0 ))
+    addConstraint(NSLayoutConstraint(item: cardView, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0 ))
 
+  }
+  
+  func configureWithCard(card: CCCard) {
+    bankImage.image = UIImage(named: (card.bank?.name)!)
+    background.backgroundColor = card.color
+    programLabel.text = card.cardProgram?.name
   }
 
 }
