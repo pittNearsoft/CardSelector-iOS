27de2337896bddd4d810b16a16a4381170cb4c53
pittNearsoft <projas@nearsoft.com>
2016-07-16 16:07:11 -0700

    Enable CardCollectionViewCell to reuse CardView

--- CardSelector/CardSelector/CardCollectionViewCell.swift
+++ CardSelector/CardSelector/CardCollectionViewCell.swift
@@ -10,33 +10,11 @@ import UIKit
 
 class CardCollectionViewCell: UICollectionViewCell {
   
+  @IBOutlet weak var card: CardView!
+  
   override func awakeFromNib() {
     super.awakeFromNib()
-    
-    
-    
-    
-    
-    let card = NSBundle.mainBundle().loadNibNamed("CardView", owner: self, options: nil).first as! CardView
     card.roundCorners()
-    //card.frame = CGRect(x: 0, y: 0, width: 338, height: 168)
-    
-
-    //card.frame = CGRect(x: 0, y: 0, width: frame.width-40, height: frame.height-80)
-    card.frame = CGRect(x: 0, y: 0, width: frame.width*(0.88), height: frame.height*(0.48))
-    
-    addSubview(card)
-    
-    
-    
-//    addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-8-[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": card]))
-//    
-//    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterX, relatedBy: .Equal, toItem: self, attribute: .CenterX, multiplier: 1, constant: 0 ) )
-//    
-//    addConstraint(NSLayoutConstraint(item: card, attribute: .CenterY, relatedBy: .Equal, toItem: self, attribute: .CenterY, multiplier: 1, constant: 0 ) )
-    
-    
-    card.center = self.center
   }
   
   static func reuseIdentifier() -> String {
