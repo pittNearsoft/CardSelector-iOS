04c061c0480d1ee12d971125e9b36e1e5789d59b
pittNearsoft <projas@nearsoft.com>
2016-07-17 23:49:05 -0700

    Configure getAvailableCards

--- CardSelector/CardSelector/CCCardViewModel.swift
+++ CardSelector/CardSelector/CCCardViewModel.swift
@@ -6,8 +6,19 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
+import Foundation
+import ObjectMapper
 
-class CCCardViewModel: NSObject {
+class CCCardViewModel {
+  let cardService = CCCardService()
+  
+  func getAvailableCards(completion: (listCards: [CCCard])-> Void, onError: (error: NSError)->Void) {
+    cardService.getAvailableCards({ (jsonCards) in
+      let cards: [CCCard] = Mapper<CCCard>().mapArray(jsonCards)!
+      completion(listCards: cards)
+    }) { (error) in
+      onError(error: error)
+    }
+  }
 
 }
