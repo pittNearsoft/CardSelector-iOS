6697ab3b532a16e5da942a6479777c8b4e7d4bb8
pittNearsoft <projas@nearsoft.com>
2016-07-17 23:36:33 -0700

    Add CCCardService

---
+++ CardSelector/CardSelector/CCCardService.swift
@@ -0,0 +1,13 @@
+//
+//  CCCardService.swift
+//  CardSelector
+//
+//  Created by projas on 7/17/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import Alamofire
+
+class CCCardService {
+
+}
