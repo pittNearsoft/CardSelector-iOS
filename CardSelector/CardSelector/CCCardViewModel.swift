27f12bb278b5900c7954b345355638a8b51c0172
pittNearsoft <projas@nearsoft.com>
2016-07-17 23:33:43 -0700

    Add CCCardViewModel

---
+++ CardSelector/CardSelector/CCCardViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CCCardViewModel.swift
+//  CardSelector
+//
+//  Created by projas on 7/17/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CCCardViewModel: NSObject {
+
+}
