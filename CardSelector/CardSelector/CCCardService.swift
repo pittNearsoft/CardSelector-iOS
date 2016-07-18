3b54fb69449e97bbca49a5f0646c70ecabe2b51e
pittNearsoft <projas@nearsoft.com>
2016-07-17 23:45:43 -0700

    Configure getAvailableCards

--- CardSelector/CardSelector/CCCardService.swift
+++ CardSelector/CardSelector/CCCardService.swift
@@ -9,5 +9,23 @@
 import Alamofire
 
 class CCCardService {
+  private let apiClient = APIClient()
+  
+  func getAvailableCards(completion: (jsonCards: [AnyObject])-> Void, onError: (error: NSError)->Void) {
+    apiClient.manager.request(CCCardRouter.getAvailableCards())
+      .CCresponseJSON { (response) in
+        switch response.result{
+        case .Success(let JSON):
+          guard let result = JSON as? [AnyObject] else{
+            onError(error: Error.error(code: -1, failureReason: "Bad json received"))
+            return
+          }
+          completion(jsonCards: result)
+          
+        case .Failure(let error):
+          onError(error: error)
+        }
+    }
+  }
 
 }
