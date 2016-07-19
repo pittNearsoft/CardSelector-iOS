034075c0e6a86c18f244f2636816adfd5216ccfa
pittNearsoft <projas@nearsoft.com>
2016-07-18 19:44:42 -0700

    Add bankCollectionView and validate delegates when cardCollectionView is invoked

--- CardSelector/CardSelector/AddCardViewController.swift
+++ CardSelector/CardSelector/AddCardViewController.swift
@@ -12,10 +12,12 @@ import SVProgressHUD
 class AddCardViewController: UIViewController {
   
   @IBOutlet weak var cardCollectionView: UICollectionView!
+  @IBOutlet weak var bankCollectionView: UICollectionView!
   
   let cardViewModel = CCCardViewModel()
   
   var listCards: [CCCard] = []
+  var listBanks: [CCBank] = []
   
   
   override func viewDidLoad() {
@@ -24,8 +26,28 @@ class AddCardViewController: UIViewController {
     cardCollectionView.dataSource = self
     cardCollectionView.delegate = self
     
-    let nibName = UINib(nibName: CardCollectionViewCell.reuseIdentifier(), bundle:nil)
-    cardCollectionView.registerNib(nibName, forCellWithReuseIdentifier: CardCollectionViewCell.reuseIdentifier())
+    bankCollectionView.dataSource = self
+    bankCollectionView.delegate = self
+    
+    let cardNibName = UINib(nibName: CardCollectionViewCell.reuseIdentifier(), bundle:nil)
+    cardCollectionView.registerNib(cardNibName, forCellWithReuseIdentifier: CardCollectionViewCell.reuseIdentifier())
+    
+    let bankNibName = UINib(nibName: BankCollectionViewCell.reuseIdentifier(), bundle:nil)
+    bankCollectionView.registerNib(bankNibName, forCellWithReuseIdentifier: BankCollectionViewCell.reuseIdentifier())
+    
+    
+    listBanks.append(CCBank(bankId: 1, name: "Chase", description: ""))
+    listBanks.append(CCBank(bankId: 2, name: "Bank Of America", description: ""))
+    listBanks.append(CCBank(bankId: 3, name: "wells", description: ""))
+    
+    listBanks.append(CCBank(bankId: 1, name: "Chase", description: ""))
+    listBanks.append(CCBank(bankId: 2, name: "Bank Of America", description: ""))
+    listBanks.append(CCBank(bankId: 3, name: "wells", description: ""))
+    
+    listBanks.append(CCBank(bankId: 1, name: "Chase", description: ""))
+    listBanks.append(CCBank(bankId: 2, name: "Bank Of America", description: ""))
+    listBanks.append(CCBank(bankId: 3, name: "wells", description: ""))
+    
     getAvailableCards()
   }
   
@@ -39,14 +61,15 @@ class AddCardViewController: UIViewController {
   }
   
   func getAvailableCards() {
-    SVProgressHUD.show()
+    
+    cardCollectionView.lock()
     cardViewModel.getAvailableCards({ (listCards) in
       self.listCards = listCards
       self.cardCollectionView.reloadData()
-      SVProgressHUD.dismiss()
+      self.cardCollectionView.unlock()
       }) { (error) in
         print(error.localizedDescription)
-        SVProgressHUD.dismiss()
+        self.cardCollectionView.unlock()
     }
   }
   
@@ -59,38 +82,79 @@ extension AddCardViewController: UICollectionViewDataSource{
   }
   
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return listCards.count
+    if collectionView == self.cardCollectionView {
+      return listCards.count
+    }else {
+      return listBanks.count
+    }
+    
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    let cell = cardCollectionView.dequeueReusableCellWithReuseIdentifier(CardCollectionViewCell.reuseIdentifier(), forIndexPath: indexPath) as! CardCollectionViewCell
     
-    cell.configureCellWithCard(listCards[indexPath.row])
+    if collectionView == self.cardCollectionView {
+      let cell = cardCollectionView.dequeueReusableCellWithReuseIdentifier(CardCollectionViewCell.reuseIdentifier(), forIndexPath: indexPath) as! CardCollectionViewCell
     
-    return cell
+      cell.configureCellWithCard(listCards[indexPath.row])
+      return cell
+    } else{
+      let cell = bankCollectionView.dequeueReusableCellWithReuseIdentifier(BankCollectionViewCell.reuseIdentifier(), forIndexPath: indexPath) as! BankCollectionViewCell
+      
+      cell.bankImage.image = UIImage(named: listBanks[indexPath.row].name)
+      return cell
+    }
+    
+    
+    
+  
   }
 }
 
 extension AddCardViewController: UICollectionViewDelegate{
   func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-    let cell = collectionView.cellForItemAtIndexPath(indexPath) as! CardCollectionViewCell
-    cell.checkImage.hidden = !cell.checkImage.hidden
+    
+    if collectionView == self.cardCollectionView {
+      let cell = collectionView.cellForItemAtIndexPath(indexPath) as! CardCollectionViewCell
+      cell.checkImage.hidden = !cell.checkImage.hidden
+    }
+    
   }
 }
 
 extension AddCardViewController: UICollectionViewDelegateFlowLayout{
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
-    return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)  }
+    
+    if collectionView == self.cardCollectionView {
+      return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+    }else{
+      return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    }
+  }
   
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAtIndex section: Int) -> CGFloat {
-    return 0
+    
+    if collectionView == self.cardCollectionView {
+      return 0
+    }else{
+      return 10
+    }
   }
   
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAtIndex section: Int) -> CGFloat {
-    return 0
+    if collectionView == self.cardCollectionView {
+      return 0
+    }else{
+      return 10
+    }
   }
   
   func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-    return CGSize(width: cardCollectionView.frame.width, height: cardCollectionView.frame.height)
+    if collectionView == self.cardCollectionView {
+      return CGSize(width: cardCollectionView.frame.width, height: cardCollectionView.frame.height)
+    }else{
+      return CGSize(width: 85, height: 70)
+    }
+    
+    
   }
 }
