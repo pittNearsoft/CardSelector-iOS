6d57effb8495fa7424831e5e2114a2651b17c594
pittNearsoft <projas@nearsoft.com>
2016-07-21 01:17:13 -0700

    Add CCProfileCard

--- CardSelector/CardSelector/CCProfileCard.swift
+++ CardSelector/CardSelector/CCProfileCard.swift
@@ -6,8 +6,27 @@
 //  Copyright © 2016 ABC. All rights reserved.
 //
 
-import UIKit
 
-class CCProfileCard: NSObject {
+import Foundation
+import ObjectMapper
 
+class CCProfileCard: Mappable {
+  
+  var card: CCCard?
+  var endingCard = -1
+  var interestRate: Double = -1
+
+  required init?(_ map: Map) {
+    
+  }
+  
+  func mapping(map: Map) {
+    card          <- map["Card"]
+    endingCard    <- map["EndingCard"]
+    interestRate  <- map["InterestRate"]
+  }
+  
+  init(){
+  
+  }
 }
