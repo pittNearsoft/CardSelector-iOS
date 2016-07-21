1984c22ec868ba99a4706f7207e2d6ed6d83be6c
pittNearsoft <projas@nearsoft.com>
2016-07-21 01:15:36 -0700

    Add gradient images and generic CC logo

---
+++ CardSelector/CardSelector/CCProfileCard.swift
@@ -0,0 +1,13 @@
+//
+//  CCProfileCard.swift
+//  CardSelector
+//
+//  Created by projas on 7/20/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CCProfileCard: NSObject {
+
+}
