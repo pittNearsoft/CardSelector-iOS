f5c7917edd41547f39cfc577c0d1a3f1fd977fdf
pittNearsoft <projas@nearsoft.com>
2016-07-22 00:35:54 -0700

    Add gender and birthday and configure inits

--- CardSelector/CardSelector/CCUser.swift
+++ CardSelector/CardSelector/CCUser.swift
@@ -10,62 +10,104 @@ import Foundation
 import GoogleSignIn
 
 class CCUser: NSObject, NSCoding {
-  var userID    = ""
-  var name      = ""
+  var userId    = ""
+  var firstName = ""
+  var lastName  = ""
   var email     = ""
+  var gender    = ""
+  var birthDate = ""
+  
   var imageUrl  = ""
   var provider  = 0
   var profileCards: [CCProfileCard] = []
   
   
-  init(userID: String, name: String, email: String, imageUrl: String, provider: Int) {
-    self.userID   = userID
-    self.name     = name
-    self.email    = email
-    self.imageUrl = imageUrl
-    self.provider = provider
+  init(userDictionary: [String: AnyObject]) {
+    self.userId     = userDictionary["userId"] as! String
+    self.firstName  = userDictionary["firstName"] as! String
+    self.lastName   = userDictionary["lastName"] as! String
+    self.email      = userDictionary["email"] as! String
+    self.gender     = userDictionary["gender"] as! String
+    self.birthDate  = userDictionary["birthDate"] as! String
+    
+    self.imageUrl   = userDictionary["imageUrl"] as! String
+    self.provider   = userDictionary["provider"] as! Int
   }
   
   init(WithGoogleUser googleUser: GIDGoogleUser) {
-    userID    = googleUser.userID
-    //tokenId   = googleUser.authentication.idToken
-    name      = googleUser.profile.name
+    userId    = googleUser.userID
+    firstName = googleUser.profile.givenName
+    lastName  = googleUser.profile.familyName
     email     = googleUser.profile.email
+    
+    
     imageUrl  = googleUser.profile.imageURLWithDimension(200).absoluteString
     provider = SignInType.Google.rawValue
   }
   
   init(WithFacebookUser facebookUser: [String: AnyObject]) {
-    userID    = facebookUser["id"] as! String
-    name      = facebookUser["name"] as! String
+    userId    = facebookUser["id"] as! String
+    firstName = facebookUser["first_name"] as! String
+    lastName  = facebookUser["last_name"] as! String
     email     = facebookUser["email"] as! String
-    provider = SignInType.Facebook.rawValue
+
+    if facebookUser["gender"] != nil {
+      gender = facebookUser["gender"] as! String
+    }
+    
+    if facebookUser["birthday"] != nil {
+      birthDate = facebookUser["birthday"] as! String
+    }
+    
+    //imageUrl = facebookUser["picture"]!["data"]!?["url"]! as! String
+    provider  = SignInType.Facebook.rawValue
   }
   
   init(WithEmail email: String) {
-    userID = NSUUID().UUIDString
-    name = "projas"
-    self.email = email
-    provider = SignInType.Email.rawValue
+    //TODO: FIX THIS
+//    userID = NSUUID().UUIDString
+//    name = "projas"
+//    self.email = email
+//    provider = SignInType.Email.rawValue
   }
   
   convenience required init?(coder decoder: NSCoder) {
-    guard let userID    = decoder.decodeObjectForKey("userID") as? String,
-          let name      = decoder.decodeObjectForKey("name") as? String,
-          let email     = decoder.decodeObjectForKey("email") as? String,
-          let imageUrl  = decoder.decodeObjectForKey("imageUrl") as? String,
-          let provider  = decoder.decodeObjectForKey("provider") as? Int
+    guard let userId      = decoder.decodeObjectForKey("userId") as? String,
+          let firstName   = decoder.decodeObjectForKey("firstName") as? String,
+          let lastName    = decoder.decodeObjectForKey("lastName") as? String,
+          let email       = decoder.decodeObjectForKey("email") as? String,
+          let gender      = decoder.decodeObjectForKey("gender") as? String,
+          let birthDate   = decoder.decodeObjectForKey("birthDate") as? String,
+      
+          let imageUrl    = decoder.decodeObjectForKey("imageUrl") as? String,
+          let provider    = decoder.decodeObjectForKey("provider") as? Int
       else{ return nil }
     
-    self.init(userID: userID, name: name, email: email, imageUrl: imageUrl, provider: provider)
+    let dictionary = [
+      "userId": userId,
+      "firstName" : firstName,
+      "lastName"  : lastName,
+      "email"     : email,
+      "gender"    : gender,
+      "birthDate" : birthDate,
+      
+      "imageUrl"  : imageUrl,
+      "provider"  : provider
+    ]
+    
+    self.init(userDictionary: dictionary as! [String : AnyObject] )
   }
   
   func encodeWithCoder(coder: NSCoder) {
-    coder.encodeObject(self.userID, forKey: "userID")
-    coder.encodeObject(self.name, forKey: "name")
-    coder.encodeObject(self.email, forKey: "email")
-    coder.encodeObject(self.imageUrl, forKey: "imageUrl")
-    coder.encodeObject(self.provider, forKey: "provider")
+    coder.encodeObject(self.userId,     forKey: "userId")
+    coder.encodeObject(self.firstName,  forKey: "firstName")
+    coder.encodeObject(self.lastName,   forKey: "lastName")
+    coder.encodeObject(self.email,      forKey: "email")
+    coder.encodeObject(self.gender,     forKey: "gender")
+    coder.encodeObject(self.birthDate,  forKey: "birthDate")
+    
+    coder.encodeObject(self.imageUrl,   forKey: "imageUrl")
+    coder.encodeObject(self.provider,   forKey: "provider")
   }
   
 }
