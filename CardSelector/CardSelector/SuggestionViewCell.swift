162d083fbda3adec874478d641ecd608f72fc69a
pittNearsoft <projas@nearsoft.com>
2016-07-23 21:38:25 -0700

    Add SuggestionViewCell

---
+++ CardSelector/CardSelector/SuggestionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SuggestionViewCell.swift
+//  CardSelector
+//
+//  Created by projas on 7/23/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class SuggestionViewCell: UITableViewCell {
+  
+  
+  @IBOutlet weak var suggestionDescription: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+
+  }
+  
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
+
+  }
+  
+}
