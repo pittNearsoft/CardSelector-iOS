790dc1ed984cd4db3bcfc7378b9663c7354fca09
pittNearsoft <projas@nearsoft.com>
2016-07-23 21:25:21 -0700

    Add CCSuggestionViewModel

---
+++ CardSelector/CardSelector/CCSuggestionViewModel.swift
@@ -0,0 +1,22 @@
+//
+//  CCSuggestionViewModel.swift
+//  CardSelector
+//
+//  Created by projas on 7/23/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+import Foundation
+import ObjectMapper
+
+class CCSuggestionViewModel {
+  let suggestionService = CCSuggestionService()
+  
+  func getSuggestionsWithUser(user: CCUser, merchant: String, completion: (listSuggestions: [String]) -> Void, onError: (error: NSError) -> Void) {
+    suggestionService.getSuggestionsWithUser(user, merchant: merchant, completion: { (jsonSuggestions) in
+      completion(listSuggestions: jsonSuggestions as! [String])
+    }) { (error) in
+      onError(error: error)
+    }
+  }
+  
+}
