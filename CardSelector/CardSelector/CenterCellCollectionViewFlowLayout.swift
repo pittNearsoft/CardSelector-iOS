aa16ebe662e2b84f2f88361e9fba02e9d34a74b8
pittNearsoft <projas@nearsoft.com>
2016-07-24 23:27:48 -0700

    Add CenterCellCollectionViewFlowLayout to center collectionViewCells

---
+++ CardSelector/CardSelector/CenterCellCollectionViewFlowLayout.swift
@@ -0,0 +1,60 @@
+//
+//  CenterCellCollectionViewFlowLayout.swift
+//  CardSelector
+//
+//  Created by projas on 7/24/16.
+//  Copyright © 2016 ABC. All rights reserved.
+//
+
+import UIKit
+
+class CenterCellCollectionViewFlowLayout: UICollectionViewFlowLayout {
+  
+  override func targetContentOffsetForProposedContentOffset(proposedContentOffset: CGPoint, withScrollingVelocity velocity: CGPoint) -> CGPoint {
+    
+    if let cv = self.collectionView {
+      
+      let cvBounds = cv.bounds
+      let halfWidth = cvBounds.size.width * 0.5;
+      let proposedContentOffsetCenterX = proposedContentOffset.x + halfWidth;
+      
+      if let attributesForVisibleCells = self.layoutAttributesForElementsInRect(cvBounds){
+        
+        var candidateAttributes : UICollectionViewLayoutAttributes?
+        for attributes in attributesForVisibleCells {
+          
+          // == Skip comparison with non-cell items (headers and footers) == //
+          if attributes.representedElementCategory != UICollectionElementCategory.Cell {
+            continue
+          }
+          
+          if let candAttrs = candidateAttributes {
+            
+            let a = attributes.center.x - proposedContentOffsetCenterX
+            let b = candAttrs.center.x - proposedContentOffsetCenterX
+            
+            if fabsf(Float(a)) < fabsf(Float(b)) {
+              candidateAttributes = attributes;
+            }
+            
+          }
+          else { // == First time in the loop == //
+            
+            candidateAttributes = attributes;
+            continue;
+          }
+          
+          
+        }
+        
+        return CGPoint(x: round(candidateAttributes!.center.x - halfWidth), y: proposedContentOffset.y)
+        
+      }
+      
+    }
+    
+    // Fallback
+    return super.targetContentOffsetForProposedContentOffset(proposedContentOffset)
+  }
+  
+}
