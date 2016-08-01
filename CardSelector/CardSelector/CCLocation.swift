b5709d02e39e8c04533b8d81acedbb36c813202c
pittNearsoft <projas@nearsoft.com>
2016-07-31 21:21:13 -0700

    Add CCLocation to get coordinate from Google places

---
+++ CardSelector/CardSelector/CCLocation.swift
@@ -0,0 +1,24 @@
+//
+//  CCLocation.swift
+//  Card Compadre
+//
+//  Created by projas on 7/31/16.
+//  Copyright © 2016 mbt. All rights reserved.
+//
+
+import UIKit
+import ObjectMapper
+
+class CCLocation: Mappable {
+  var latitude: Double?
+  var longitude: Double?
+  
+  required init?(_ map: Map) {
+    
+  }
+  
+  func mapping(map: Map) {
+    latitude            <- map["geometry.location.lat"]
+    longitude          <- map["geometry.location.lng"]
+  }
+}
